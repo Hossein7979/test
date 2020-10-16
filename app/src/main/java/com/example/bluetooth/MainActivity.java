2c848a151ac50e29737d6ba569e5487620af8a60
Your Name <you@example.com>
2020-10-16 18:13:19 +0330

    sec

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -38,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
     final int SEND_MESSAGE=2;
     sendReceive receive;
     Client client;
-    int git=0;
+    //commit
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
